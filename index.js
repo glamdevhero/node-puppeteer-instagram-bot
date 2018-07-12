e579a9eb54c1be0e810354ee7c573a8b65753e67
nicolas lierman <nicolas.lierman@multiminds.eu>
2018-07-12 18:55:15 +0200

    set bot interval

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let bot = require('./src/puppeteer');
 let cnf = require('./config/config.json');
 
 bot();
-//setInterval(bot, cnf.settings.run_every_x_hours * 3600000);
+setInterval(bot, cnf.settings.run_every_x_hours * 3600000);
 
